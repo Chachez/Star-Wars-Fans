12fe272a0659a85c88c4dcf1a8069016b267eaba
Chachez <chachamichael89@gmail.com>
2022-07-06 09:12:24 +0300

    Bug fix

--- src/redux/actions/filmActions.js
+++ src/redux/actions/filmActions.js
@@ -6,7 +6,7 @@ const { REACT_APP_URL } = process.env;
 export const searchFilm = (data) => async (dispatch) => {
   try {
     const res = await axios.get(
-      `${REACT_APP_URL}films/?search=${data.searchParams}`
+      `${REACT_APP_URL}/films/?search=${data.searchParams}`
     );
     dispatch({ type: filmActions.FILM_DETAILS, payload: res.data });
     dispatch({ type: filmActions.SEARCH, payload: data.searchParams });
